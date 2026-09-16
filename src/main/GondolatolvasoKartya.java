b36306e7c158a80a971f712a4d175ce623ccd477
Stolár-Németh Villő (SZF_N_2025_2027) <stolar.nemeth.villo@diak.szamalk-szalezi.hu>
2026-09-16 16:31:13 +0200

    a feladat alapja

---
+++ src/main/GondolatolvasoKartya.java
@@ -0,0 +1,41 @@
+package main;
+
+public class GondolatolvasoKartya {
+
+    static String[] pakli = new String[20];
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            kirajzol(); // 1tömb
+            valasztas(); // Scanner
+            keveres(); // középre
+        }
+        
+        ezVolt(); //11., az az a középső
+        
+    }
+    
+    private static void feltoltes(){
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
+    }
+
+    private static void kirajzol() {
+       
+    }
+
+    private static void valasztas() {
+        
+    }
+
+    private static void keveres() {
+        
+    }
+
+    private static void ezVolt() {
+                
+    }
+    
+    
+    
+}
